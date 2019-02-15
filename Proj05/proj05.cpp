032f8f13bcc848bb548446916e37976098b96cb3
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-02-14 22:55:08 -0500

    Update proj05.cpp

--- Proj05/proj05.cpp
+++ Proj05/proj05.cpp
@@ -5,110 +5,151 @@ using std::cout; using std::cin; using std::endl; using std::boolalpha;  // C++ 
 #include <string>  // the head File
 using std::string; using std::to_string;  // C++ STL
 
-#include <cmath>
+#include <cmath>  // the head File
 
 string lower_case(string s) {
+
     string str;
+
     for (char c : s)
-        str += tolower(c);
+        str += std::to_string(tolower(c));
+
     return str;
 }
 
 string to_binary(char c) {
+
     string empty;
+
     if (islower(c)) {
+
         int num = c - 'a';
         string str;
+
         while (num) {
             string s = to_string(num % 2);
             num = num / 2;
             str = s + str;
         }
+
         str = "00000" + str;
         string remainder(str, str.size()-5);
+
         return remainder;
     }
+
     else
         return empty;
 }
 
 char from_binary(string bit_str) {
+
     if (bit_str.size() == 5) {
+
         double exp = 4, num = 0;
         char bit;
+
         for (char c : bit_str) {
+
             if (c == '0' or c == '1')
                 num = num + (int(c)-48) * pow(2.0, exp);
+
             --exp;
         }
-        bit = 'a' + num;
+
+        bit = static_cast<char>('a' + num);
+
         if (islower(bit))
             return bit;
     }
+
     return NULL;
 }
 
 bool check_message(string plaintext, string secret_message) {
+
     int num_plaintext = 0, num_secret_message = 0;
+
     for (char c : plaintext) {
         if (isalnum(c)) {
             ++num_plaintext;
         }
     }
+
     for (char c : secret_message) {
         if (isalnum(c)) {
             ++num_secret_message;
         }
     }
+
     if (num_plaintext > 5 * num_secret_message)
         return true;
+
     return false;
 }
 
 string encode(string plaintext, string secret_message) {
+
     plaintext = lower_case(plaintext);
     secret_message = lower_case(secret_message);
+
     string bin;
+
     if (check_message(plaintext, secret_message)) {
+
         for (char c : secret_message) {
             bin += to_binary(c);
         }
-        int index = 0;
+
+        auto index = 0;
+
         for (char c : bin) {
+
             while (!isalpha(plaintext[index]))
                 ++index;
+
             if (c == '1')
-                plaintext[index] = toupper(plaintext[index]);
+                plaintext[index] = static_cast<char>(toupper(plaintext[index]));
+
             ++index;
         }
+
         return plaintext;
     }
+
     return "Error";
 }
 
 string decode(string to_decode) {
+
     string new_decode, bin;
+
     for (char c : to_decode) {
         if (isalpha(c))
             new_decode += c;
     }
+
     for (char c : new_decode) {
         if (islower(c))
             bin += '0';
         else
             bin += '1';
     }
+
     string code;
-    int index = 0;
+    unsigned long index = 0;
+
     for (auto i = 0; i < bin.size() / 5; ++i) {
         char c;
         c = from_binary(bin.substr(index, 5));
         code += c;
         index += 5;
     }
+
     return code;
 }
 
+// main is below. You cannot modify main
 int main() {
 
     cout << boolalpha;
@@ -160,6 +201,8 @@ int main() {
             cout << decode(encoded) << endl;
             break;
         }  // of case 6
+
+        default:break;
 
     }  // of switch
 }  // of main
