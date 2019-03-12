48d8f914cd5d3ff52822769890fc6fd344ce9267
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-03-12 12:11:12 -0400

    Update main.cpp

--- Proj07/main.cpp
+++ Proj07/main.cpp
@@ -13,18 +13,25 @@ using std::endl;
 #include <map>
 using std::map;
 
+#include <fstream>
+using std::ifstream;
+
 #include "functions.h"
 
 int main() {
     vector<string> v = split("This is a test.");
     ostringstream os;
     print_vector(os, v) << endl;
-
-    string test = clean_word("A1B2C3xyz!!");
-
-    map_type m = {{"a", 1}, {"b", 2}, {"c", 3}};
+    map_type m;
+    bool result = process_words(m, "/Users/one/Documents/CSE232/Project07/file1.txt");
     print_map(os, m) << endl;
     string s = os.str();
-
     std::cout << s;
+
+    map_type m1, m2;
+    bool result1 = process_words(m1, "/Users/one/Documents/CSE232/Project07/file1.txt");
+    bool result2 = process_words(m2, "/Users/one/Documents/CSE232/Project07/file2.txt");
+    double sim1 = jac_similarity(m1, m2);
+    double sim2 = cosine_similarity_tf(m1, m2);
+    std::cout << sim2;
 }
