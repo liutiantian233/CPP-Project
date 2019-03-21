0d7e973164a4cddcd87218463634fd6c50c8744a
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-03-21 12:09:53 -0400

    Update functions.h

--- Proj08/functions.h
+++ Proj08/functions.h
@@ -1,26 +1,31 @@
+// the head begin
 #ifndef PROJECT08_FUNCTIONS_H
 #define PROJECT08_FUNCTIONS_H
 
-#include <vector>
-using std::vector;
+#include <vector>  // the head File
+using std::vector;  // C++ STL
 
-#include <string>
-using std::string;
+#include <string>  // the head File
+using std::string;  // C++ STL
 
-#include <iostream>
-using std::cin; using std::cout; using std::endl;
+#include <iostream>  // the head File
+using std::cin; using std::cout; using std::endl;  // C++ STL
 
+// Defining the new struct
 struct Image {
 
 public:  // could be private, testing easier as public
     vector<vector<long>> pixels_;
+
+    // Defining the default
     long height_ = 0;
     long width_ = 0;
     long max_val_ = 0;
-    Image convolve(vector<vector<long>> mask, long divisor = 1, long whiten = 0) const;
+    
+    Image convolve(vector<vector<long>> mask, long divisor = 1, long whiten = 0) const;  // Defining the default
 
-public:
-    Image() = default;
+public:  // this is public function
+    Image() = default;  // Defining the default
     explicit Image(string f_name);
     void write_image(string f_name) const;
     Image sharpen() const;
