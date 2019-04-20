d416d794bd612d2b4b1bfcbf43cdb377dffd84d6
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-04-20 05:07:39 -0400

    Update class.h

--- Proj11/class.h
+++ Proj11/class.h
@@ -1,33 +1,73 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Computer Project #11   MVM List
+//
+//  Define the head File "iostream" "initializer_list" "algorithm" "utility" "stdexcept"
+//    use the C++ Standard Library headers
+//    the "algorithm" "utility" "stdexcept" "initializer_list" are C++ tools
+//
+//  Defining class public function in Element:
+//    Element(K key, initializer_list<V> values): copy into the array values_ and set count_ correctly.
+//                                                Sets next_ to nullptr (no next Element in a list yet).
+//    bool operator == (const Element&): Behavior is identical to the previous project.
+//                                       Doesn't need to account for next_.
+//    friend ostream& operator << (ostream&, Element&): Behavior is identical to the previous project.
+//    bool operator < (const K&): compares the key passed to the element's key.
+//    Element(const Element& other): Copy ctor. Copies count_ and values_. Sets next_ to nullptr.
+//                                   Remember that values_ is a fixed-size array.
+//                                   You know how to copy that, you just did it in project 10.
+//
+//  Defining class public function in MVM:
+//    MVM(initializer_list<Element<K, V>): Behavior is identical to the previous project. However,
+//                                         you are now adding Elements, in order as found in the initializer_list,
+//                                         into a linked list. Same idea, different data structure.
+//    pair<Element*, Element*> find_key(K key): We can no longer use lower_bound as we mentioned.
+//    However, having a singly linked list means that finding only the first greater value is of limited use.
+//    MVM find_value(V value): creates an MVM where each key is a key that has value in its values_,
+//                             and the only value associated with each key is value.
+//    size_t size(): Returns num_keys_.
+//    bool add(string key, string value): Behavior is identical to previous project,
+//                                        with the exception being that you no longer need to call grow(),
+//                                        just dynamically allocate a new Element as necessary.
+//    bool remove_key(K key): Behavior is identical to previous project.
+//    MVM remove_value(V value): creates and returns a MVM of the same format used in find_value.
+//    friend ostream& operator << (ostream&, MVM&): print an MVM.
+//    ~MVM(): Destructor. Deletes any allocated memory as necessary.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #ifndef PROJECT11_CLASS_H
 #define PROJECT11_CLASS_H
 
-#include <iostream>
-using std::cout; using std::endl; using std::boolalpha; using std::ostream;
+#include <iostream>  // the head File
+using std::cout; using std::endl; using std::boolalpha; using std::ostream;  // C++ STL
 
-#include <initializer_list>
-using std::initializer_list;
+#include <initializer_list>  // the head File
+using std::initializer_list;  // C++ STL
 
-#include <algorithm>
-using std::max; using std::copy; using std::swap;
+#include <algorithm>  // the head File
+using std::max; using std::copy; using std::swap;  // C++ STL
 
-#include <utility>
-using std::make_pair; using std::pair;
+#include <utility>  // the head File
+using std::make_pair; using std::pair;  // C++ STL
 
-#include <stdexcept>
-using std::runtime_error;
+#include <stdexcept>  // the head File
+using std::runtime_error;  // C++ STL
 
+// the const about the size
 const static size_t element_array_size = 5;
 
 template <typename K, typename V>
+// the struct about Element
 struct Element {
 
 public:
     K key_;
     V values_[element_array_size];
-    Element* next_ = nullptr;
+    Element * next_ = nullptr;
 
     // Optional
-    // Element* previous = nullptr;
+    // Element * previous = nullptr;
 
     size_t count_ = 0;
 
@@ -36,6 +76,7 @@ public:
     Element(const Element& other);
     bool operator == (const Element&) const;
     bool operator < (const K&) const;
+
     friend ostream& operator << (ostream& oss, Element& element) {
         // Code for the ostream operator goes here
         oss << element.key_ << ":";
@@ -52,10 +93,13 @@ public:
 // Code for Element functions goes here
 template <typename K, typename V>
 Element<K, V>::Element(K key, initializer_list<V> values) {
+    // copy into the array values_ and set count_ correctly.
     key_ = key;
-    count_ = values.size();
-    copy(values.begin(), values.end(), values_);
-    next_ = nullptr;
+    if (values.size() <= element_array_size) {
+        count_ = values.size();
+        copy(values.begin(), values.end(), values_);
+        next_ = nullptr;
+    }
 }
 
 template <typename K, typename V>
@@ -63,7 +107,6 @@ bool Element<K, V>::operator == (const Element &element) const {
     // if key is element and count
     if (key_ == element.key_ and count_ == element.count_) {
         for (auto i = 0; i < count_; ++i)
-
             if (values_[i] != element.values_[i])
                 return false;
         return true;
@@ -74,16 +117,22 @@ bool Element<K, V>::operator == (const Element &element) const {
 
 template <typename K, typename V>
 Element<K, V>::Element(const Element &other) {
+    // Copy ctor. Copies count_ and values_. Sets next_ to nullptr.
+    // Remember that values_ is a fixed-size array. You know how to copy that, you just did it in project 10.
     key_ = other.key_;
     count_ = other.count_;
     copy(other.values_, other.values_ + other.count_, values_);
-    Element* next_ = nullptr;
+    next_ = nullptr;
 }
 
 template <typename K, typename V>
 bool Element<K, V>::operator < (const K &key) const {
-    return key_ < key;
+    return key_ < key;  // return the bigger key
 }
+
+
+
+
 
 template <typename K, typename V>
 class MVM {
@@ -92,7 +141,7 @@ public:
     Element<K, V> * data_head_ = nullptr;
 
     // Optional
-    // Element<K, V> * data_tail_ = nullptr;
+    Element<K, V> * data_tail_ = nullptr;
     // size_t num_elements_ = 0;
 
     size_t num_keys_ = 0;
@@ -106,8 +155,8 @@ public:
     MVM(initializer_list<Element<K, V>>);
 
     // Rule of Three
-    // MVM(const MVM& other);
-    // MVM& operator = (const MVM& other);
+    MVM(const MVM& other);
+    MVM<K, V>& operator = (const MVM& other);
     ~MVM();
 
     // Other Functions
@@ -131,16 +180,69 @@ public:
 
 // Code for MVM functions goes here
 template <typename K, typename V>
+MVM<K, V>::MVM(const MVM &other) {
+    num_keys_ = other.num_keys_;
+    if (data_head_ != nullptr) {
+        auto * point = data_head_;  // make a point to data_head_
+        num_keys_ = 0;
+
+        // while loop to get all point
+        while (point != nullptr) {
+            data_head_ = data_head_->next_;
+            delete point;  // delete the point
+            point = data_head_;
+        }
+        data_head_ = nullptr;
+        data_tail_ = nullptr;
+    }
+    if (other.data_head_ != nullptr) {
+        data_head_ = new Element<K,V> (*(other.data_head_));
+        auto * point1 = data_head_;
+        auto * point2 = other.data_head_;
+        data_tail_ = point1;
+        while (point2->next_ != nullptr) {
+            data_tail_ = point1;
+            point1 = new Element<K,V> (*(point2->next_));
+            data_tail_->next_ = point1;
+            point2 = point2->next_;
+        }
+        data_tail_ = point1;
+    } else {
+        data_head_ = nullptr;
+        data_tail_ = nullptr;
+    }
+}
+
+template <typename K, typename V>
+MVM<K, V>& MVM<K, V>::operator = (const MVM &other) {
+    // Makes this have the same values as other.
+    MVM<K, V> mvm(other);
+    num_keys_ = mvm.num_keys_;
+    //use the copy ctor to make a copy of other that you can then use to swap data members with the local MVM.
+    swap(data_head_, mvm.data_head_);
+    swap(data_tail_, mvm.data_tail_);
+    return mvm;
+}
+
+template <typename K, typename V>
 size_t MVM<K, V>::size() {
+    // Returns num_keys_.
     return num_keys_;
 }
 
 template <typename K, typename V>
 MVM<K, V>::MVM(initializer_list<Element<K, V>> element) {
-    data_head_ = new Element<K, V> (*element.begin());
+    // Behavior is identical to the previous project.
+    if (element.size() != 0)
+        data_head_ = new Element<K, V> (*element.begin());
+
+    // make a point to data_head_
     auto * point = data_head_;
+
+    // for loop to add the element
     for (auto i = 1; i < element.size(); ++i) {
-        point->next_ = new Element<K, V> (*(element.begin() + i));
+        data_tail_ = new Element<K, V> (*(element.begin() + i));
+        point->next_ = data_tail_;
         point = point->next_;
     }
     num_keys_ = element.size();
@@ -148,122 +250,107 @@ MVM<K, V>::MVM(initializer_list<Element<K, V>> element) {
 
 template <typename K, typename V>
 MVM<K, V>::~MVM() {
-    delete data_head_;
+    // Destructor. Deletes any allocated memory as necessary.
+    auto * point = data_head_;  // make a point to data_head_
+    num_keys_ = 0;
+
+    // while loop to get all point
+    while (point != nullptr) {
+        data_head_ = data_head_->next_;
+        delete point;  // delete the point
+        point = data_head_;
+    }
+    data_head_ = nullptr;
+    data_tail_ = nullptr;
 }
 
 template <typename K, typename V>
 pair<Element<K, V>*, Element<K, V>*> MVM<K, V>::find_key(K key) {
+    // We can no longer use lower_bound as we mentioned.
+
+    // the .first points to the Element just behind (just previous to) the Element pointed to by .second
     Element<K, V> * first = nullptr;
-    Element<K, V> * second = nullptr;
-    auto * point = data_head_;
-    for (auto i = 0; i < num_keys_; ++i) {
-        if (point->key_ < key) {
-            first = point;
-            point = point->next_;
-        } else {
-            second = point;
+
+    // the .second points to the Element whose key_ is either equal to or just greater than the parameter key.
+    Element<K, V> * second = data_head_;
+    if (second == nullptr)  // if the empty
+        return pair<Element<K, V>*, Element<K, V>*> (first, second);
+    while (second->key_ < key) {
+        first = second;
+        second = second->next_;
+        if (second == nullptr)
             break;
-        }
     }
-    pair<Element<K, V>*, Element<K, V>*> pair1 (first, second);
-    return pair1;
+    return pair<Element<K, V>*, Element<K, V>*> (first, second);
 }
 
 template <typename K, typename V>
 MVM<K, V> MVM<K, V>::find_value(V value) {
+    // finds all keys where value is located.
     MVM<K, V> mvm;
-    auto * point = data_head_;
-    for (auto i = 0; i < num_keys_; ++i) {
-        for (auto j = 0; j < point->count_; ++j) {
-            if (point->values_[j] == value) {
-                mvm.num_keys_ += 1;
-                auto * point1 = new Element<K, V> (point->key_, {value});
-                if (mvm.data_head_ == nullptr)
-                    mvm.data_head_ = point1;
-                else {
-                    auto * point2 = mvm.data_head_;
-                    while (point2->next_ != nullptr)
-                        point2 = point2->next_;
-                    point2->next_ = point1;
-                }
-            }
-        }
-        point = point->next_;
-    }
+    // creates an MVM where each key is a key that has value in its values_
+    // and the only value associated with each key is value.
+    for (auto * point = data_head_; point != nullptr; point = point->next_)
+        for (auto item = 0; item < point->count_; ++item)
+            if (point->values_[item] == value)
+                mvm.add(point->key_, value);
+    // returns the new MVM.
     return mvm;
 }
 
 template <typename K, typename V>
 bool MVM<K, V>::add(K key, V value) {
-    Element<K, V> element(key, {value});
-    if (find_key(key).first == nullptr and find_key(key).second == nullptr) {
-        data_head_ = new Element<K, V> (element);
-        num_keys_ += 1;
-    }
-    if (find_key(key).first == nullptr and find_key(key).second != nullptr) {
-        if (find_key(key).second->key_ == key) {
-            auto f = std::find(find_key(key).second->values_,
-                               find_key(key).second->values_ + find_key(key).second->count_, value);
-            if (f != find_key(key).second->values_ + find_key(key).second->count_
-            or find_key(key).second->count_ == element_array_size)
-                return false;
-            data_head_->values_[data_head_->count_] = value;
-            data_head_->count_ += 1;
-        } else {
-            num_keys_ += 1;
-            auto * point = new Element<K, V> (element);
-            point->next_ = data_head_;
-            data_head_ = point;
+    // Behavior is identical to previous project
+    // with the exception being that you no longer need to call grow()
+    // just dynamically allocate a new Element as necessary.
+    auto found = find_key(key);
+
+    // if the second can to find
+    if (found.second != nullptr and found.second->key_ == key) {
+        // to find the value
+        auto f = std::find(found.second->values_, found.second->values_ + found.second->count_, value);
+        if (f != found.second->values_ + found.second->count_ or found.second->count_ == element_array_size)
+            return false;
+        found.second->values_[found.second->count_++] = value;
+    } else {
+        auto data = new Element<K, V> (key, {value});
+
+        // if the found is empty
+        if (found.first == nullptr and found.second == nullptr)
+            data_head_ = data;
+        // if the found is first
+        if (found.first == nullptr and found.second != nullptr) {
+            data_head_ = data;
+            data_head_->next_ = found.second;
         }
-    }
-    if (find_key(key).first != nullptr and find_key(key).second == nullptr) {
-        num_keys_ += 1;
-        auto * point1 = new Element<K, V> (element);
-        auto * point2 = data_head_;
-        while (point2->next_ != nullptr)
-            point2 = point2->next_;
-        point2->next_ = point1;
-    }
-    if (find_key(key).first != nullptr and find_key(key).second != nullptr) {
-        if (find_key(key).second->key_ == key) {
-            auto f = std::find(find_key(key).second->values_,
-                               find_key(key).second->values_ + find_key(key).second->count_, value);
-            if (f != find_key(key).second->values_ + find_key(key).second->count_
-            or find_key(key).second->count_ == element_array_size)
-                return false;
-            auto * point = data_head_;
-            while (point->key_ != key)
-                point = point->next_;
-            point->values_[point->count_] = value;
-            point->count_ += 1;
-        } else {
-            num_keys_ += 1;
-            auto * point1 = data_head_;
-            auto * point2 = new Element<K, V>;
-            auto * point0 = new Element<K, V> (element);
-            while (key > point1->key_) {
-                point2 = point1;
-                point1 = point1->next_;
-            }
-            point2->next_ = point0;
-            point0->next_ = point1;
+        // if the found is end
+        if (found.first != nullptr and found.second == nullptr) {
+            found.first->next_ = data;
         }
+        // if the found is middle
+        if (found.first != nullptr and found.second != nullptr) {
+            found.first->next_ = data;
+            found.first->next_->next_ = found.second;
+        }
+        num_keys_ += 1;
     }
     return true;
 }
 
 template <typename K, typename V>
 bool MVM<K, V>::remove_key(K key) {
-    if (find_key(key).second->key_ == key) {
+    // Behavior is identical to previous project.
+    auto found = find_key(key);
+
+    // use the find_key
+    if (found.second->key_ == key) {
         num_keys_ -= 1;
-        if (data_head_->key_ == key)
-            data_head_ = data_head_->next_;
-        else {
-            while (data_head_->next_->key_ != key)
-                data_head_ = data_head_->next_;
-            if (data_head_->next_->key_ == key)
-                data_head_->next_ = data_head_->next_->next_;
-        }
+        // if the found.first is nullptr
+        if (found.first == nullptr)
+            data_head_ = found.second->next_;
+        else
+            found.first->next_ = found.second->next_;
+        delete found.second;  // delete the second
         return true;
     }
     return false;
@@ -271,8 +358,10 @@ bool MVM<K, V>::remove_key(K key) {
 
 template <typename K, typename V>
 MVM<K, V> MVM<K, V>::remove_value(V value) {
+    // finds all keys where value is located, and removes value from the values_ array.
     auto * point1 = data_head_;
-    auto mvm = find_value(value);
+    auto mvm = find_value(value);  // use the find_value function to handle most of the work.
+    // creates and returns a MVM of the same format used in find_value.
     auto * point2 = mvm.data_head_;
     for (auto i = 0; i < num_keys_; ++i) {
         if (point2 == nullptr)
